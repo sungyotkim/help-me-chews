a7a5674e273132e836edee3b9ed197369aa546da
sungyotkim <sungyotkim@gmail.com>
2022-10-14 14:26:19 -0400

    add styling for dropdown of reviews

--- frontend/src/components/Business/BusinessReviewsSort.jsx
+++ frontend/src/components/Business/BusinessReviewsSort.jsx
@@ -1,9 +1,12 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./BusinessReviewSort.css";
 
 const BusinessReviewSort = () => {
   const [searchReviewsAnimate, setSearchReviewsAnimate] = useState(false);
+  const [showHelpSort, setShowHelpSort] = useState(false);
+  const [showRatingSort, setShowRatingSort] = useState(false);
   const ref = useRef(null);
+  const node = useRef();
 
   const handleFocus = () => {
     setSearchReviewsAnimate(!searchReviewsAnimate);
@@ -17,11 +20,75 @@ const BusinessReviewSort = () => {
     ref.current.focus();
   };
 
+  const handleHelpSortClick = () => {
+    if (showHelpSort) {
+      setShowHelpSort(false);
+    } else {
+      setShowHelpSort(true);
+    }
+  };
+
+  const clickOutside = () => {
+    setShowHelpSort(false);
+    setShowRatingSort(false);
+  };
+
+  const handleRatingSortClick = () => {
+    if (showRatingSort) {
+      setShowRatingSort(false);
+    } else {
+      setShowRatingSort(true);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", clickOutside);
+    return () => {
+      document.removeEventListener("mousedown", clickOutside);
+    };
+  }, [showHelpSort, showRatingSort]);
+
   return (
     <div className="business-reviews-sorting-options-container">
       <div className="sorting-btns-container">
-        <div className="help-sort">Help me Sort</div>
-        <div className="filter-by-rating-sort">Filter by Rating</div>
+        <div className="help-sort">
+          <div className="help-sort-btn" onClick={handleHelpSortClick}>
+            Help me Sort
+            <svg width={16} height={16}>
+              <path d="M8 10.25a.746.746 0 01-.525-.215l-3.055-3a.75.75 0 011.05-1.07L8 8.449l2.53-2.484a.75.75 0 011.05 1.07l-3.055 3A.746.746 0 018 10.25z"></path>
+            </svg>
+          </div>
+          {showHelpSort && (
+            <div className="help-sort-dropdown">
+              <div className="active-help-sort">Help me Sort</div>
+              <div>Newest First</div>
+              <div>Oldest First</div>
+              <div>Highest Rated</div>
+              <div>Lowest Rated</div>
+            </div>
+          )}
+        </div>
+        <div className="filter-by-rating-sort">
+          <div
+            className="filter-by-rating-sort-btn"
+            onClick={handleRatingSortClick}
+          >
+            Filter by Rating
+            <svg width={16} height={16}>
+              <path d="M8 10.25a.746.746 0 01-.525-.215l-3.055-3a.75.75 0 011.05-1.07L8 8.449l2.53-2.484a.75.75 0 011.05 1.07l-3.055 3A.746.746 0 018 10.25z"></path>
+            </svg>
+          </div>
+          {showRatingSort && (
+            <div className="rating-sort-dropdown">
+              <div className="active-rating-sort">5 stars</div>
+              <div>4 stars</div>
+              <div>3 stars</div>
+              <div>2 stars</div>
+              <div>1 star</div>
+              <div>All ratings</div>
+            </div>
+          )}
+        </div>
       </div>
       <div className="review-search-bar">
         <input
