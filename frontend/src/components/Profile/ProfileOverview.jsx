56f4a357f253a214e5f0fcda1bd961d306e43f9a
sungyotkim <sungyotkim@gmail.com>
2022-10-18 14:13:12 -0400

    adjust sizing, finish profile overview placeholder

--- frontend/src/components/Profile/ProfileOverview.jsx
+++ frontend/src/components/Profile/ProfileOverview.jsx
@@ -3,7 +3,16 @@ import "./ProfileOverview.css";
 const ProfileOverview = () => {
   return (
     <>
-      <div>This is overview</div>
+      <div className="profile-overview-container">
+        <div className="profile-overview-header">Friend requests</div>
+        <div className="profile-overview-header-content">
+          No new friend requests at this time.
+        </div>
+        <div className="profile-recent-activity-header">Recent Activity</div>
+        <div className="profile-recent-activity-content">
+          We don't have any recent activity for you right now.
+        </div>
+      </div>
     </>
   );
 };
