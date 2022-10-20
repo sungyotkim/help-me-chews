88f42b8305809af64f24f91de5513e9812e882bc
sungyotkim <sungyotkim@gmail.com>
2022-10-20 15:06:04 -0400

    add time out for search

--- frontend/src/components/SearchResults/LoadingResults.jsx
+++ frontend/src/components/SearchResults/LoadingResults.jsx
@@ -1,27 +1,51 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import "./LoadingResults.css";
 
 const LoadingResults = ({ counter }) => {
-  return (
-    <>
-      {counter.map((number) => {
-        return (
-          <div className="loading-results-container" key={number}>
-            <div className="loading-results-left-contaier">
-              <div className="loading-image-placeholder"></div>
+  const [timedOut, setTimedOut] = useState(false);
+
+  useEffect(() => {
+    const noticeTimeout = setTimeout(() => {
+      setTimedOut(true);
+    }, 3000);
+
+    return () => {
+      clearTimeout(noticeTimeout);
+    };
+  }, [counter]);
+
+  if (!timedOut) {
+    return (
+      <>
+        {counter.map((number) => {
+          return (
+            <div className="loading-results-container" key={number}>
+              <div className="loading-results-left-contaier">
+                <div className="loading-image-placeholder"></div>
+              </div>
+              <div className="loading-results-right-container">
+                <div className="loading-results-first-right-placeholder"></div>
+                <div className="loading-results-second-right-placeholder"></div>
+                <div className="loading-results-third-right-placeholder"></div>
+                <div className="loading-results-fourth-right-placeholder"></div>
+                <div className="loading-results-fifth-right-placeholder"></div>
+                <div className="loading-results-sixth-right-placeholder"></div>
+              </div>
             </div>
-            <div className="loading-results-right-container">
-              <div className="loading-results-first-right-placeholder"></div>
-              <div className="loading-results-second-right-placeholder"></div>
-              <div className="loading-results-third-right-placeholder"></div>
-              <div className="loading-results-fourth-right-placeholder"></div>
-              <div className="loading-results-fifth-right-placeholder"></div>
-              <div className="loading-results-sixth-right-placeholder"></div>
-            </div>
-          </div>
-        );
-      })}
-    </>
-  );
+          );
+        })}
+      </>
+    );
+  } else if (timedOut) {
+    return (
+      <>
+        <div className="timed-out-container">
+          Sorry, no results found. Please try a different search criteria.
+        </div>
+      </>
+    );
+  }
 };
 
 export default LoadingResults;
