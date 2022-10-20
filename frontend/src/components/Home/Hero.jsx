45f6b54e9874f8876171572d8cc3b627c5e840bb
sungyotkim <sungyotkim@gmail.com>
2022-10-20 11:20:33 -0400

    fix slides

--- frontend/src/components/Home/Hero.jsx
+++ frontend/src/components/Home/Hero.jsx
@@ -7,30 +7,77 @@ import HeroSlide from "./HeroSlide";
 const bogliasco = "https://i.imgur.com/Gu5Cznz.jpg";
 const giauPass = "https://i.imgur.com/8IuucQZ.jpg";
 const countyClare = "https://i.imgur.com/idjXzVQ.jpg";
-// const craterRock = "https://i.imgur.com/8DYumaY.jpg";
+const craterRock = "https://i.imgur.com/8DYumaY.jpg";
 
 const HeroContainer = () => {
-  let backgroundImages = [bogliasco, giauPass, countyClare];
+  let backgroundImages = [bogliasco, giauPass, countyClare, craterRock];
   const [displayImage, setDisplayImage] = useState(backgroundImages[0]);
-  const [slide, setSlide] = useState(0);
+  const [displaySlide, setDisplaySlide] = useState(0);
+  const [heroBarOne, setHeroBarOne] = useState(0);
+  const [heroBarTwo, setHeroBarTwo] = useState(0);
+  const [heroBarThree, setHeroBarThree] = useState(0);
+  const [heroBarFour, setHeroBarFour] = useState(0);
+  // const [opacity, setOpacity] = useState(0.7);
+  let tracker = 0;
   let i = 0;
+  // let heroBarOne = 10;
 
   useEffect(() => {
     let interval = setInterval(() => {
-      if (i === 2) {
+      if (i === 3) {
         i = 0;
       } else {
         i += 1;
       }
 
       setDisplayImage(backgroundImages[i]);
-      setSlide(i);
+      setDisplaySlide(i);
     }, 6000);
 
     return () => {
       clearInterval(interval);
     };
   }, []);
+
+  useEffect(() => {
+    if (displaySlide === 0) {
+      let firstSlideInterval = setInterval(() => {
+        setHeroBarOne((old) => old + 1);
+        tracker++;
+        if (tracker === 100) {
+          clearInterval(firstSlideInterval);
+          tracker = 0;
+        }
+      }, 57);
+    } else if (displaySlide === 1) {
+      let secondSlideInterval = setInterval(() => {
+        setHeroBarTwo((old) => old + 1);
+        tracker++;
+        if (tracker === 100) {
+          clearInterval(secondSlideInterval);
+          tracker = 0;
+        }
+      }, 57);
+    } else if (displaySlide === 2) {
+      let thirdSlideInterval = setInterval(() => {
+        setHeroBarThree((old) => old + 1);
+        tracker++;
+        if (tracker === 100) {
+          clearInterval(thirdSlideInterval);
+          tracker = 0;
+        }
+      }, 57);
+    } else if (displaySlide === 3) {
+      let fourthSlideInterval = setInterval(() => {
+        setHeroBarFour((old) => old + 1);
+        tracker++;
+        if (tracker === 100) {
+          clearInterval(fourthSlideInterval);
+          tracker = 0;
+        }
+      }, 57);
+    }
+  }, [displaySlide]);
 
   return (
     <>
@@ -45,7 +92,43 @@ const HeroContainer = () => {
           fromSearch={{ fromSearch: false }}
           fromBusiness={{ fromBusiness: false }}
         />
-        <HeroSlide slide={slide} />
+        <div className="hero-slide-1">
+          <div className="hero-bars">
+            <div className="hero-bar-clickable">
+              <div className="hero-bar">
+                <div
+                  className="hero-bar-fill"
+                  style={{ height: `${heroBarOne}%` }}
+                ></div>
+              </div>
+            </div>
+            <div className="hero-bar-clickable">
+              <div className="hero-bar">
+                <div
+                  className="hero-bar-fill"
+                  style={{ height: `${heroBarTwo}%` }}
+                ></div>
+              </div>
+            </div>
+            <div className="hero-bar-clickable">
+              <div className="hero-bar">
+                <div
+                  className="hero-bar-fill"
+                  style={{ height: `${heroBarThree}%` }}
+                ></div>
+              </div>
+            </div>
+            <div className="hero-bar-clickable">
+              <div className="hero-bar">
+                <div
+                  className="hero-bar-fill"
+                  style={{ height: `${heroBarFour}%` }}
+                ></div>
+              </div>
+            </div>
+          </div>
+          <HeroSlide displaySlide={displaySlide} />
+        </div>
       </div>
     </>
   );
--- frontend/src/components/Home/HeroSlide.jsx
+++ frontend/src/components/Home/HeroSlide.jsx
@@ -1,102 +1,11 @@
-import { useEffect, useState } from "react";
-
-const HeroSlide = ({ slide }) => {
-  const [heroBarOne, setHeroBarOne] = useState(0);
-  const [heroBarTwo, setHeroBarTwo] = useState(0);
-  const [heroBarThree, setHeroBarThree] = useState(0);
-  const [heroBarFour, setHeroBarFour] = useState(0);
-  let tracker = 0;
-
-  useEffect(() => {
-    setHeroBarOne(0);
-    setHeroBarTwo(0);
-    setHeroBarThree(0);
-    setHeroBarFour(0);
-    if (slide === 0) {
-      let firstSlideInterval = setInterval(() => {
-        setHeroBarOne((old) => old + 1);
-        tracker++;
-        if (tracker === 100) {
-          clearInterval(firstSlideInterval);
-          tracker = 0;
-        }
-      }, 57);
-    } else if (slide === 1) {
-      setHeroBarOne(100);
-      let firstSlideInterval = setInterval(() => {
-        setHeroBarTwo((old) => old + 1);
-        tracker++;
-        if (tracker === 100) {
-          clearInterval(firstSlideInterval);
-          tracker = 0;
-        }
-      }, 57);
-    } else if (slide === 2) {
-      setHeroBarOne(100);
-      setHeroBarTwo(100);
-      let firstSlideInterval = setInterval(() => {
-        setHeroBarThree((old) => old + 1);
-        tracker++;
-        if (tracker === 100) {
-          clearInterval(firstSlideInterval);
-          tracker = 0;
-        }
-      }, 57);
-    } else if (slide === 3) {
-      setHeroBarOne(100);
-      setHeroBarTwo(100);
-      setHeroBarThree(100);
-      let firstSlideInterval = setInterval(() => {
-        setHeroBarFour((old) => old + 1);
-        tracker++;
-        if (tracker === 100) {
-          clearInterval(firstSlideInterval);
-          tracker = 0;
-        }
-      }, 57);
-    }
-  }, [slide]);
+const HeroSlide = ({ displaySlide }) => {
+  const content = ["slide 1", "slide 2", "slide 3", "slide 4"];
+  const btn = ["1", "2", "3", "4"];
 
   return (
-    <div className="hero-slide-1">
-      <div className="hero-bars">
-        <div className="hero-bar-clickable">
-          <div className="hero-bar">
-            <div
-              className="hero-bar-fill"
-              style={{ height: `${heroBarOne}%` }}
-            ></div>
-          </div>
-        </div>
-        <div className="hero-bar-clickable">
-          <div className="hero-bar">
-            <div
-              className="hero-bar-fill"
-              style={{ height: `${heroBarTwo}%` }}
-            ></div>
-          </div>
-        </div>
-        <div className="hero-bar-clickable">
-          <div className="hero-bar">
-            <div
-              className="hero-bar-fill"
-              style={{ height: `${heroBarThree}%` }}
-            ></div>
-          </div>
-        </div>
-        <div className="hero-bar-clickable">
-          <div className="hero-bar">
-            <div
-              className="hero-bar-fill"
-              style={{ height: `${heroBarFour}%` }}
-            ></div>
-          </div>
-        </div>
-      </div>
-      <div className="hero-slide-content">
-        <div>This is a slide</div>
-        <div className="hero-slide-btn">Btn</div>
-      </div>
+    <div className="hero-slide-content">
+      <div>{content[displaySlide]}</div>
+      <div className="hero-slide-btn">{btn[displaySlide]}</div>
     </div>
   );
 };
