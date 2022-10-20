eefe47da52a187d98c77016374dca206817058b1
sungyotkim <sungyotkim@gmail.com>
2022-10-20 11:40:58 -0400

    allow onclick to change hero

--- frontend/src/components/Home/Hero.jsx
+++ frontend/src/components/Home/Hero.jsx
@@ -1,4 +1,3 @@
-// import HeroSlider, { Slide } from "hero-slider";
 import React, { useEffect, useState } from "react";
 import "./Hero.css";
 import Header from "../Header/Header";
@@ -17,10 +16,15 @@ const HeroContainer = () => {
   const [heroBarTwo, setHeroBarTwo] = useState(0);
   const [heroBarThree, setHeroBarThree] = useState(0);
   const [heroBarFour, setHeroBarFour] = useState(0);
+  const [clicked, setClicked] = useState(false);
   // const [opacity, setOpacity] = useState(0.7);
   let tracker = 0;
   let i = 0;
+  const [newIndex, setNewIndex] = useState();
   // let heroBarOne = 10;
+  if (newIndex) {
+    i = newIndex;
+  }
 
   useEffect(() => {
     let interval = setInterval(() => {
@@ -34,12 +38,23 @@ const HeroContainer = () => {
       setDisplaySlide(i);
     }, 6000);
 
+    if (clicked) {
+      setClicked(false);
+      clearInterval(interval);
+      setInterval(interval, 6000);
+    }
+
     return () => {
       clearInterval(interval);
     };
-  }, []);
+  }, [clicked]);
 
   useEffect(() => {
+    tracker = 0;
+    setHeroBarOne(0);
+    setHeroBarTwo(0);
+    setHeroBarThree(0);
+    setHeroBarFour(0);
     if (displaySlide === 0) {
       let firstSlideInterval = setInterval(() => {
         setHeroBarOne((old) => old + 1);
@@ -51,6 +66,7 @@ const HeroContainer = () => {
       }, 57);
     } else if (displaySlide === 1) {
       let secondSlideInterval = setInterval(() => {
+        setHeroBarOne(100);
         setHeroBarTwo((old) => old + 1);
         tracker++;
         if (tracker === 100) {
@@ -60,6 +76,8 @@ const HeroContainer = () => {
       }, 57);
     } else if (displaySlide === 2) {
       let thirdSlideInterval = setInterval(() => {
+        setHeroBarOne(100);
+        setHeroBarTwo(100);
         setHeroBarThree((old) => old + 1);
         tracker++;
         if (tracker === 100) {
@@ -69,6 +87,9 @@ const HeroContainer = () => {
       }, 57);
     } else if (displaySlide === 3) {
       let fourthSlideInterval = setInterval(() => {
+        setHeroBarOne(100);
+        setHeroBarTwo(100);
+        setHeroBarThree(100);
         setHeroBarFour((old) => old + 1);
         tracker++;
         if (tracker === 100) {
@@ -78,6 +99,15 @@ const HeroContainer = () => {
       }, 57);
     }
   }, [displaySlide]);
+
+  const handleClick = (e, index) => {
+    e.preventDefault();
+    setNewIndex(index);
+    setDisplayImage(backgroundImages[index]);
+    setDisplaySlide(index);
+    setClicked(true);
+    tracker = 100;
+  };
 
   return (
     <>
@@ -94,7 +124,10 @@ const HeroContainer = () => {
         />
         <div className="hero-slide-1">
           <div className="hero-bars">
-            <div className="hero-bar-clickable">
+            <div
+              className="hero-bar-clickable"
+              onClick={(e) => handleClick(e, 0)}
+            >
               <div className="hero-bar">
                 <div
                   className="hero-bar-fill"
@@ -102,7 +135,10 @@ const HeroContainer = () => {
                 ></div>
               </div>
             </div>
-            <div className="hero-bar-clickable">
+            <div
+              className="hero-bar-clickable"
+              onClick={(e) => handleClick(e, 1)}
+            >
               <div className="hero-bar">
                 <div
                   className="hero-bar-fill"
@@ -110,7 +146,10 @@ const HeroContainer = () => {
                 ></div>
               </div>
             </div>
-            <div className="hero-bar-clickable">
+            <div
+              className="hero-bar-clickable"
+              onClick={(e) => handleClick(e, 2)}
+            >
               <div className="hero-bar">
                 <div
                   className="hero-bar-fill"
@@ -118,7 +157,10 @@ const HeroContainer = () => {
                 ></div>
               </div>
             </div>
-            <div className="hero-bar-clickable">
+            <div
+              className="hero-bar-clickable"
+              onClick={(e) => handleClick(e, 3)}
+            >
               <div className="hero-bar">
                 <div
                   className="hero-bar-fill"
