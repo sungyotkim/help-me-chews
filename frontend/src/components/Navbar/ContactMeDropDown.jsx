8d2b64d7f6be5b7202b1504aded3dff62b804ab5
sungyotkim <sungyotkim@gmail.com>
2022-10-19 23:20:20 -0400

    tidy up dead links

--- frontend/src/components/Navbar/ContactMeDropDown.jsx
+++ frontend/src/components/Navbar/ContactMeDropDown.jsx
@@ -1,5 +1,24 @@
 import "./ContactMeDropdown.css";
 import { useState } from "react";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+import { styled } from "@mui/material/styles";
+
+const CustomToolTip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(() => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: "#2d2e2f",
+    fontSize: 12,
+    boxShadow: "rgb(0 0 0 / 15%) 0 0 18px",
+    borderRadius: "4px",
+    padding: "12px 16px",
+    position: "relative",
+    fontFamily: "'Poppins', Arial, Helvetica, sans-serif;",
+  },
+  [`& .${tooltipClasses.arrow}`]: {
+    color: "#2d2e2f",
+  },
+}));
 
 const ContactMeDropdown = ({ styleBlack }) => {
   const [isHovering, setIsHovering] = useState(false);
@@ -32,12 +51,14 @@ const ContactMeDropdown = ({ styleBlack }) => {
       {isHovering && (
         <div className="contact-me-dropdown-revealed">
           <div className="contact-me-left-dropdown">
-            <div className="contact-me-left-dropdown-option">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path d="M19,4H5A3,3,0,0,0,2,7V17a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V7A3,3,0,0,0,19,4Zm-.67,2L12,10.75,5.67,6ZM19,18H5a1,1,0,0,1-1-1V7.25l7.4,5.55a1,1,0,0,0,.6.2,1,1,0,0,0,.6-.2L20,7.25V17A1,1,0,0,1,19,18Z" />
-              </svg>
-              <div>Email</div>
-            </div>
+            <CustomToolTip title="sungyotkim@gmail.com" arrow placement="left">
+              <div className="contact-me-left-dropdown-option no-cursor">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path d="M19,4H5A3,3,0,0,0,2,7V17a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V7A3,3,0,0,0,19,4Zm-.67,2L12,10.75,5.67,6ZM19,18H5a1,1,0,0,1-1-1V7.25l7.4,5.55a1,1,0,0,0,.6.2,1,1,0,0,0,.6-.2L20,7.25V17A1,1,0,0,1,19,18Z" />
+                </svg>
+                <div>Email</div>
+              </div>
+            </CustomToolTip>
             <div className="contact-me-left-dropdown-option">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
                 <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
@@ -46,13 +67,15 @@ const ContactMeDropdown = ({ styleBlack }) => {
             </div>
           </div>
           <div className="contact-me-right-dropdown">
-            <div className="contact-me-right-dropdown-option">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-                <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />{" "}
-                <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
-              </svg>
-              <div>Phone</div>
-            </div>
+            <CustomToolTip title="718-702-2156" arrow placement="right">
+              <div className="contact-me-right-dropdown-option no-cursor">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                  <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />{" "}
+                  <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                </svg>
+                <div>Phone</div>
+              </div>
+            </CustomToolTip>
             <div className="contact-me-right-dropdown-option">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
