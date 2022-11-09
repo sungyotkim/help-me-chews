b85cb8387972c7bb42ad12cb72e64b407b189fce
sungyotkim <sungyotkim@gmail.com>
2022-11-09 18:07:00 -0500

    adjust business search context

--- frontend/src/contexts/BusinessSearchContext.js
+++ frontend/src/contexts/BusinessSearchContext.js
@@ -1,3 +1,71 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const BusinessSearchContext = createContext({});
+
+export const BusinessSearchProvider = ({ children }) => {
+  const [term, setTerm] = useState("null");
+  const [location, setLocation] = useState("New York, NY");
+  const [searchedTerm, setSearchedTerm] = useState("null");
+  const [searchedLocation, setSearchedLocation] = useState("New York, NY");
+  const [radius, setRadius] = useState("null");
+  const [price, setPrice] = useState([]);
+  const [openNow, setOpenNow] = useState("null");
+  const [genderNeutralBathrooms, setGenderNeutralBathrooms] = useState("null");
+  const [wheelchairAccessible, setWheelchairAccessible] = useState("null");
+  const [businessId, setBusinessId] = useState("");
+  const [businessResults, setBusinessResults] = useState({});
+  const [limit, setLimit] = useState(5);
+  const [offset, setOffset] = useState(0);
+  const [hotAndNew, setHotAndNew] = useState("null");
+  const [currentBusinessResults, setCurrentBusinessResults] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [resultReviews, setResultReviews] = useState([]);
+  const [allResultReviews, setAllResultReviews] = useState([]);
+
+  return (
+    <>
+      <BusinessSearchContext.Provider
+        value={{
+          term,
+          setTerm,
+          location,
+          setLocation,
+          radius,
+          setRadius,
+          price,
+          setPrice,
+          openNow,
+          setOpenNow,
+          genderNeutralBathrooms,
+          setGenderNeutralBathrooms,
+          wheelchairAccessible,
+          setWheelchairAccessible,
+          businessId,
+          setBusinessId,
+          businessResults,
+          setBusinessResults,
+          limit,
+          setLimit,
+          offset,
+          setOffset,
+          searchedTerm,
+          setSearchedTerm,
+          searchedLocation,
+          setSearchedLocation,
+          hotAndNew,
+          setHotAndNew,
+          currentBusinessResults,
+          setCurrentBusinessResults,
+          loading,
+          setLoading,
+          resultReviews,
+          setResultReviews,
+          allResultReviews,
+          setAllResultReviews,
+        }}
+      >
+        {children}
+      </BusinessSearchContext.Provider>
+    </>
+  );
+};
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,8 +7,7 @@ import { Provider } from "react-redux";
 import configureStore from "./store";
 import csrfFetch from "./store/csrf";
 import * as sessionActions from "./store/session";
-import { BusinessSearchContext } from "./contexts/BusinessSearchContext";
-import { useState } from "react";
+import { BusinessSearchProvider } from "./contexts/BusinessSearchContext";
 
 const store = configureStore();
 
@@ -19,71 +18,13 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 function Root() {
-  const [term, setTerm] = useState("null");
-  const [location, setLocation] = useState("New York, NY");
-  const [searchedTerm, setSearchedTerm] = useState("null");
-  const [searchedLocation, setSearchedLocation] = useState("New York, NY");
-  const [radius, setRadius] = useState("null");
-  const [price, setPrice] = useState([]);
-  const [openNow, setOpenNow] = useState("null");
-  const [genderNeutralBathrooms, setGenderNeutralBathrooms] = useState("null");
-  const [wheelchairAccessible, setWheelchairAccessible] = useState("null");
-  const [businessId, setBusinessId] = useState("");
-  const [businessResults, setBusinessResults] = useState({});
-  const [limit, setLimit] = useState(5);
-  const [offset, setOffset] = useState(0);
-  const [hotAndNew, setHotAndNew] = useState("null");
-  const [currentBusinessResults, setCurrentBusinessResults] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [resultReviews, setResultReviews] = useState([]);
-  const [allResultReviews, setAllResultReviews] = useState([]);
-
   return (
     <Provider store={store}>
-      <BusinessSearchContext.Provider
-        value={{
-          term,
-          setTerm,
-          location,
-          setLocation,
-          radius,
-          setRadius,
-          price,
-          setPrice,
-          openNow,
-          setOpenNow,
-          genderNeutralBathrooms,
-          setGenderNeutralBathrooms,
-          wheelchairAccessible,
-          setWheelchairAccessible,
-          businessId,
-          setBusinessId,
-          businessResults,
-          setBusinessResults,
-          limit,
-          setLimit,
-          offset,
-          setOffset,
-          searchedTerm,
-          setSearchedTerm,
-          searchedLocation,
-          setSearchedLocation,
-          hotAndNew,
-          setHotAndNew,
-          currentBusinessResults,
-          setCurrentBusinessResults,
-          loading,
-          setLoading,
-          resultReviews,
-          setResultReviews,
-          allResultReviews,
-          setAllResultReviews,
-        }}
-      >
+      <BusinessSearchProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </BusinessSearchContext.Provider>
+      </BusinessSearchProvider>
     </Provider>
   );
 }
